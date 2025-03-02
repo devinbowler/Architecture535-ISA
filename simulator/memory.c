7be5552fe1d514963f7ba5f4a69212217574f06b
Devin Bowler <devinbowler@gmail.com>
2025-03-01 22:16:20 -0500

    Starting to work on  for now cache will come after this is working, or owen can work on cache while I do this.

---
+++ simulator/memory.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define some variables that will be used for memeory.
+#define DRAM_SIZE 60000
+#define DRAM_DELAY 100
+
+
+// A function to write into memory at a immediate address.
+
+
+// A function to read from a immediate address in memeory.
+
+
+// A function to clear memory fully.
+
+
+// A function to print off memory, but only the lines that have addresses.
+
+
+// Testing function, this is just for testing, later this will come from the tests file.
+int main(void){
+  printf("DRAM_SIZE: %d\n", DRAM_SIZE);
+
+  return 0;
+}
