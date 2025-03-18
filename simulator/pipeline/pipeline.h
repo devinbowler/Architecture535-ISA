25f05cd40a2ffde0d42dd17f6320019490a9353f
Owen <ozone8@proton.me>
2025-03-18 15:21:28 -0400

    Added starter code

---
+++ simulator/pipeline/pipeline.h
@@ -0,0 +1,8 @@
+#ifndef PIPELINE_H
+#define PIPELINE_H
+
+void execute(char[] instruction);
+void memory_access(char[] instruction);
+void write_back(char[] instruction);
+
+#endif
---
+++ simulator/pipeline/write_back.c
@@ -0,0 +1,9 @@
+#include "pipeline.h"
+
+/**
+ * @brief Implements the write back stage of the pipeline
+ * @param instruction the instruction to be written
+ */
+void write_back(char[] instruction) {
+
+}
