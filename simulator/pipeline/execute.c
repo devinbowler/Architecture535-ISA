1383c664f4c86a5e9400b574099c7bd9e53dfdb6
devin bowler <devin@devin.devin>
2025-04-01 11:25:41 -0400

    Pipeline branch creation.

---
+++ simulator/pipeline/execute.c
@@ -0,0 +1,9 @@
+#include "pipeline.h"
+
+/**
+ * @brief Implements the execute stage of the pipeline. This includes ALU operations and address calculations
+ * @param instruction the instruction to be executed
+ */
+void execute(char[] instruction) {
+
+}
---
+++ simulator/pipeline/memory_access.c
@@ -0,0 +1,9 @@
+#include "pipeline.h"
+
+/**
+ * @brief Implements the memory access stage of the pipeline. This will just use our existing cache and DRAM functions
+ * @param instruction the instruction to access memory
+ */
+void memory_access(char[] instruction) {
+
+}
